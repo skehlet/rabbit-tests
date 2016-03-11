c4c5aeb1d7f7d4aa4d09daa6a1ab349c37a21c2d
Steve Kehlet <stevek@mirthcorp.com>
2016-03-11 09:53:37 -0800

    added timeout

--- rabbit-rpc/rabbit_rpc.js
+++ rabbit-rpc/rabbit_rpc.js
@@ -7,6 +7,7 @@ var logger = require('log4js').getLogger(module.filename);
 var EXCHANGE_NAME = 'stevek';
 var ee = new EventEmitter();
 var rabbit;
+var RPC_TIMEOUT = 10 * 1000;
 
 module.exports = {
     rpc: rpc
@@ -19,7 +20,6 @@ function responseConsumer(message) {
     } catch (err) {
         obj = err;
     }
-    logger.debug('[' + message.properties.correlationId + '] ->', obj);
     ee.emit(message.properties.correlationId, obj);
 }
 
@@ -45,7 +45,6 @@ function getRabbit() {
 }
 
 function publishToRabbit(correlationId, name, args) {
-    logger.debug('[' + correlationId + ']', name + '(' + args.join(', ')  + ') ->');
     return getRabbit().spread(function(connection, channel, exchange, responseQueue) {
         return channel.publish(exchange.exchange, name, new Buffer(JSON.stringify(args)), {
             correlationId: correlationId,
@@ -62,8 +61,20 @@ function publishToRabbit(correlationId, name, args) {
 function rpc(name /*, ...*/) {
     var args = Array.prototype.slice.call(arguments, 1);
     return new Promise(function (resolve, reject) {
+        var startTime = new Date();
         var correlationId = nodeUuid();
-        ee.once(correlationId, resolve);
+        var handler = function (results) {
+            clearTimeout(timeout);
+            logger.debug('[' + correlationId + ']', results, '[' + ((new Date()) - startTime) + 'ms]');
+            resolve(results);
+        };
+        var timeoutHandler = function () {
+            ee.removeListener(correlationId, handler);
+            reject(new Error('RPC request ' + name + '(' + args.join(', ') + ') timed out after ' + RPC_TIMEOUT + 'ms'));
+        };
+        var timeout = setTimeout(timeoutHandler, RPC_TIMEOUT);
+        ee.once(correlationId, handler);
+        logger.debug('[' + correlationId + ']', name + '(' + args.join(', ')  + ')');
         publishToRabbit(correlationId, name, args).catch(reject);
     });
 }
--- rabbit-rpc/stevek.js
+++ rabbit-rpc/stevek.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var Promise = require('bluebird');
 var rabbit_rpc = require('./rabbit_rpc');
 
 var promises = [];
@@ -8,9 +9,15 @@ promises.push(rabbit_rpc.rpc('fib', 1));
 promises.push(rabbit_rpc.rpc('fib', 5));
 promises.push(rabbit_rpc.rpc('fib', 30));
 promises.push(rabbit_rpc.rpc('fib', 15));
-Promise.all(promises).then(function(results) {
-    console.log('results', results);
+
+Promise.all(promises.map(function(promise) {
+    return promise.reflect();
+})).each(function(inspection) {
+    if (inspection.isFulfilled()) {
+        console.log('results:', inspection.value());
+    } else {
+        console.log('error:', inspection.reason());
+    }
+}).finally(function() {
     process.exit(0);
-}).catch(function(err) {
-    console.log('Caught error:', err);
 });
